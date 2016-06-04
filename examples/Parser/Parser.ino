d5a5ff17d89862600bc6e83c074285c5c6a92779
Pascal Vizeli <pascal.vizeli@syshack.ch>
2016-06-04 16:41:36 +0200

    parser demo

--- examples/Parser/Parser.ino
+++ examples/Parser/Parser.ino
@@ -45,3 +45,39 @@ void setup()
         Serial.println(cmdParser.getCmdParamUpper(2));
     }
 }
+
+void loop()
+{
+    // use own buffer from serial input
+    CmdBuffer<32> myBuffer;
+
+    Serial.println("Start Reading");
+
+    // Read line from Serial until timeout
+    if (cmdBuffer.readFromSerial(&Serial, 30000)) {
+
+        if (cmdParser.parseCmd(&myBuffer)) {
+            Serial.print("Line have readed: ");
+            Serial.println(myBuffer.getStringFromBuffer());
+
+            Serial.print("Command: ") Serial.println(cmdParser.getCommand());
+
+            Serial.print("Size of parameter: ");
+            Serial.println(cmdParser.getParamCount());
+
+            for (size_t i = 0; i <= cmdParser.getParamCount(); i++) {
+
+                Serial.print("Param ");
+                Serial.print(i);
+                Serial.print(": ");
+                Serial.println(cmdParser.getCmdParam(i));
+            }
+
+        } else {
+            Serial.println("Parser error!");
+        }
+
+    } else {
+        Serial.println("TIMEOUT!");
+    }
+}
